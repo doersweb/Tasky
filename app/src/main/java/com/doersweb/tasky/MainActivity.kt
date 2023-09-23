c08c6d7af83dd2c80c0ba360912961cd23d6d40c
doersweb <doersweb@gmail.com>
2023-09-23 14:57:46 +0530

    Initial commit

---
+++ app/src/main/java/com/doersweb/tasky/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.doersweb.tasky
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.doersweb.tasky.ui.theme.TaskyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskyTheme {
+
+            }
+        }
+    }
+}
