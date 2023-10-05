9d9f4f52c942b62d63b5e8c1d97b8ea512cb565a
doersweb <doersweb@gmail.com>
2023-10-05 12:14:47 +0530

    added remaining routes

--- app/src/main/java/com/doersweb/tasky/core/navigation/Routes.kt
+++ app/src/main/java/com/doersweb/tasky/core/navigation/Routes.kt
@@ -4,4 +4,9 @@ object Routes {
     const val SPLASH = "splash"
     const val LOGIN = "login"
     const val REGISTER = "register"
+    const val AGENDA = "agenda"
+    const val AGENDA_DETAIL = "agenda_detail"
+    const val AGENDA_EDIT = "agenda_edit"
+    const val EDIT_DESCRIPTION = "edit_description"
+    const val EDIT_TITLE = "edit_title"
 }
