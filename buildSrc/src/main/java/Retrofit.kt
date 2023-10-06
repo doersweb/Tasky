b96543b2e335613dd77ce558bbb5500c2bdfbbae
doersweb <doersweb@gmail.com>
2023-10-06 09:31:20 +0530

    removed unused dependencies

--- buildSrc/src/main/java/Retrofit.kt
+++ buildSrc/src/main/java/Retrofit.kt
@@ -2,7 +2,6 @@ object Retrofit {
     private const val version = "2.9.0"
     const val retrofit = "com.squareup.retrofit2:retrofit:$version"
     const val moshiConverter = "com.squareup.retrofit2:converter-moshi:$version"
-    const val gsonConverter = "com.squareup.retrofit2:converter-gson"
 
     private const val okhttpVersion = "5.0.0-alpha.11"
     const val okHttp = "com.squareup.okhttp3:okhttp:$okhttpVersion"
